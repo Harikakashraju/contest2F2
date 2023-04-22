dbad1d8f219b7038166d2730d79e974b1e131d50
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-04-22 22:38:04 +0530

    Add files via upload

---
+++ aprilcontest2/index.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("inputs");
+
+const arr = [{id:1,name:""john"",age:""18"",profession:""developer""},{id:2, name:""jack"",age:""20"", profession:""developer""},{id:3, name:""karen"", age:""19"",profession:""admin""}];
+
+input.innerText = {id:1,name:""john"",age:""18"",profession:""developer""};
